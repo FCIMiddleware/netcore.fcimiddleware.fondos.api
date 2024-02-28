b2097a695d7bc3cef1bf6b5a893e622308293b55
Marco Espagnac <36200726+mespagnac@users.noreply.github.com>
2024-02-27 23:00:43 -0300

    add features on getall
    getall add new features order and search an page

--- src/netcore.fcimiddleware.fondos.application/Features/Shared/Queries/PaginationBaseQuery.cs
+++ src/netcore.fcimiddleware.fondos.application/Features/Shared/Queries/PaginationBaseQuery.cs
@@ -4,7 +4,12 @@
     {
         public string? Search { get; set; }
         public string? Sort { get; set; }
-        public int PageIndex { get; set; }
-        public int PageSize { get; set; }
+        public int PageIndex { get; set; } = 1;
+        private int _pageSize = 3;
+        private const int MaxPageSize = 50;
+        public int PageSize { 
+            get => _pageSize; 
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value; 
+        }
     }
 }
